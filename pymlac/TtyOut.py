3b57b8441b34a3467067aa0c7404f2759c248aef
Ross Wilson <rzzzwilson@gmail.com>
2015-07-15 11:33:58 +0700

    Rough testing of TTY output

--- pymlac/TtyOut.py
+++ pymlac/TtyOut.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 
 """
 Emulate the Output TTY (TTYOUT).
@@ -22,50 +22,40 @@ cycle_count = 0
 state = DEVICE_NOT_READY
 
 
-def init():
-    global filename, open_file, open_file, cycle_count, state
+class TtyOut(object):
 
-    filename = None
-    open_file = None
-    open_file = 0
-    cycle_count = 0
-    state = DEVICE_NOT_READY
+    def __init__(self):
+        self.filename = None
+        self.open_file = None
+        self.cycle_count = 0
+        self.state = DEVICE_NOT_READY
 
-def mount(fname):
-    global filename, open_file, open_file, cycle_count, state
+    def mount(self, fname):
+        self.filename = fname
+        self.open_file = open(self.filename, 'w')
+        self.state = DEVICE_READY
 
-    filename = fname
-    open_file = open(filename, 'w')
-    state = DEVICE_READY
+    def dismount(self):
+        if self.open_file:
+            self.open_file.close()
+        self.filename = None
+        self.open_file = None
+        self.state = DEVICE_NOT_READY
 
-def dismount():
-    global filename, open_file, open_file, cycle_count, state
+    def write(self, char):
+        if self.open_file:
+            self.open_file.write(char)
+            self.cycle_count = DEVICE_NOT_READY_CYCLES
 
-    if open_file:
-        open_file.close()
-    filename = None
-    open_file = None
-    state = DEVICE_NOT_READY
+    def ready(self):
+        return (self.state != DEVICE_NOT_READY)
 
-def write(char):
-    global filename, open_file, open_file, cycle_count, state
+    def clear(self):
+        self.state = DEVICE_NOT_READY
 
-    open_file.write(char)
-    cycle_count = DEVICE_NOT_READY_CYCLES
-
-def ready():
-    return (state != DEVICE_NOT_READY)
-
-def clear():
-    global filename, open_file, open_file, cycle_count, state
-
-    state = DEVICE_NOT_READY
-
-def tick(cycles):
-    global filename, open_file, open_file, cycle_count, state
-
-    if (state == DEVICE_NOT_READY):
-        cycle_count -= cycles
-        if cycle_count <= 0:
-            state = DEVICE_READY
+    def tick(self, cycles):
+        if (self.state == DEVICE_NOT_READY):
+            self.cycle_count -= cycles
+            if self.cycle_count <= 0:
+                self.state = DEVICE_READY
 
---
+++ pymlac/test_TTYOUT.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Test the pymlac TTY output device.
+
+Usage: test_TTYOUT.py <options>
+
+where <options> is zero or more of:
+          -h    print this help and stop
+"""
+
+
+import TtyOut
+
+
+# count of errors found
+ErrorCount = 0
+
+# module global constants
+TtyFilename = '_#TTY#_.ptp'
+
+
+def error(msg):
+    global ErrorCount
+
+    ErrorCount += 1
+
+    print(msg)
+
+def create_tty_file(filename):
+    """Create a TTY file."""
+    
+    # create a test file
+    with open(filename, 'wb') as fd:
+        # leader
+        for _ in range(16):
+            fd.write(chr(0))
+    
+        for v in range(1, 33):
+            fd.write(chr(v))
+    
+def no_mounted_file():
+    ttyout = TtyOut.TtyOut()
+
+    # write without mounting
+    val = ttyout.write(0)
+
+    # dismount
+    ttyout.dismount()
+
+def mount_dismount(filename):
+    """Just mount then dismount file."""
+
+    print('mount_dismount: filename=%s' % str(filename))
+    ttyout = TtyOut.TtyOut()
+    ttyout.mount(filename)
+    ttyout.dismount()
+
+def write_tty(filename):
+    """Mount a file and write to it."""
+
+    ttyout = TtyOut.TtyOut()
+    ttyout.mount(filename)
+
+    # write leader
+    count = 0
+    while count < 16:
+        while not ttyout.ready():
+            ttyout.tick(1)
+        byte = ttyout.write(chr(0))
+        ttyout.clear()
+        count += 1
+
+    print('wrote %d bytes of leader' % count)
+
+    # write body
+    count = 0
+    while count < 32:
+        while not ttyout.ready():
+            ttyout.tick(1)
+        ttyout.write(chr(1))
+        ttyout.clear()
+        count += 1
+    
+    print('%d bytes of body' % count)
+    
+    # now dismount the file
+    ttyout.dismount()
+
+def main():
+    """Test the TTY out device."""
+
+    # test device with no mounted file
+    no_mounted_file()
+
+    # just mount, then dismount
+    mount_dismount(TtyFilename)
+
+    # test reading from mounted device
+    write_tty(TtyFilename)
+
+    # print number of errors
+    print('\n%d errors' % ErrorCount)
+
+    return ErrorCount
+
+
+################################################################################
+
+if __name__ == '__main__':
+    import sys
+    import getopt
+
+    def usage(msg=None):
+        if msg:
+            print('*'*60)
+            print(msg)
+            print('*'*60)
+        print(__doc__)
+
+    # handle command line options
+    try:
+        (opts, args) = getopt.gnu_getopt(sys.argv, "h", ["help"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(10)
+    
+    for (opt, arg) in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit(0)
+
+    # we should have NO parameters
+    if len(args) != 1:
+        bad_args = ''
+        for a in args[1:]:
+            bad_args += ' ' + str(a)
+        usage('Unrecognized param(s): %s' % bad_args)
+        sys.exit(10)
+
+    # run the tests
+    sys.exit(main())
+    
