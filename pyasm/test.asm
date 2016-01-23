6b55c1cac880e4ffb10f32b7da512ac4d8fb6fa4
Ross Wilson <rzzzwilson@gmail.com>
2016-01-23 20:34:31 +0700

    Improved test code

--- pyasm/test.asm
+++ pyasm/test.asm
@@ -2,9 +2,12 @@
         org     0000100
 
 start   law     10          ; comment
+        lac     start2      ; another comment
         hlt
 
         org     0200
-start2  lac     0100
+start2  
+        lac     0100
+        hlt
 
         end     start
