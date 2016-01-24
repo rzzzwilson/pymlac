2a2760d962869e3af2494454c7075da9966a8994
Ross Wilson <rzzzwilson@gmail.com>
2016-01-24 11:53:30 +0700

    Added expression 'value' field

--- pyasm/test.asm
+++ pyasm/test.asm
@@ -5,7 +5,7 @@ start   law     10          ; comment
         lac     start2      ; another comment
         hlt
 
-        org     0200
+        org     0200 + 1
 start2  
         lac     0100
         hlt
