eee4670bdcb0ab373c53e38731b9f66ea713d48c
Ross Wilson <rzzzwilson@gmail.com>
2016-01-30 16:46:49 +0700

    Assembler working, more testing required

--- pyasm/test2.asm
+++ pyasm/test2.asm
@@ -2,16 +2,20 @@
         org     0100
 
 start   law     10
-        lac     start2      ; comment
+        lac     *start2     ; comment
         lac     .-2
         hlt
 
 fred    equ     2           ; EQU
 
-        org     . + 010
+        org     01100
 start2  
         lac     start + 2   ; comment
-string  data    'ascii'
+offset  data    start - 3
+        ascii   'xxxxxxxx'
+        ascii   'xxxxxxx'
+        ascii   "xx"
+        ascii   'x'
 end     hlt
 
         end     start
