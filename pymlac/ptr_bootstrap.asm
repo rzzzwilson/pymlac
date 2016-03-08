b81021931e11ab40ba6398d3e6df599d5413f234
Ross Wilson <rzzzwilson@gmail.com>
2016-03-08 12:25:12 +0700

    Dumped PTR bootstrap for debugging

---
+++ pymlac/ptr_bootstrap.asm
@@ -0,0 +1,35 @@
+; PTR bootstrap code from loading.pdf (LOADING THE PDS-1)
+        org     040
+        data    0060077      ; 040
+        data    0020010      ; 040
+        data    0104076      ; 040
+        data    0020020      ; 040
+        data    0001061      ; 040
+        data    0100011      ; 040
+        data    0002400      ; 040
+        data    0010046      ; 040
+        data    0001051      ; 040
+        data    0074075      ; 040
+        data    0010045      ; 040
+        data    0002400      ; 040
+        data    0010053      ; 040
+        data    0001051      ; 040
+        data    0003003      ; 040
+        data    0003003      ; 040
+        data    0003002      ; 040
+        data    0102400      ; 040
+        data    0010061      ; 040
+        data    0002400      ; 040
+        data    0010063      ; 040
+        data    0001051      ; 040
+        data    0120010      ; 040
+        data    0102400      ; 040
+        data    0010067      ; 040
+        data    0100011      ; 040
+        data    0030020      ; 040
+        data    0010053      ; 040
+        data    0110076      ; 040
+        data    0000002      ; 040
+        data    0037700      ; 040
+        data    0037677      ; 040
+        end     
