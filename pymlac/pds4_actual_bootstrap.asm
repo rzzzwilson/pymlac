0f7a84b201d37bfdcec54c74a2cb9b17c0ba7860
Ross Wilson <rzzzwilson@gmail.com>
2016-03-10 17:00:07 +0700

    Disassembly of bootstraps from docs/PNG

---
+++ pymlac/pds4_actual_bootstrap.asm
@@ -0,0 +1,38 @@
+; ??? bootstrap code from file 'Sydney_University_PDS-4_Rom.png'
+; the values here are the 'Actual Contents' code
+        org     040
+
+        data    0101037      ; 040
+        data    0100000      ; 041
+        data    0101031      ; 042
+        data    0003001      ; 043
+        data    0002002      ; 044
+        data    0010042      ; 045
+        data    0004073      ; 046
+        data    0101035      ; 047
+        data    0004100      ; 050
+        data    0101036      ; 051
+        data    0006001      ; 052
+        data    0101034      ; 053
+        data    0101030      ; 054
+        data    0102002      ; 055
+        data    0010054      ; 056
+        data    0010065      ; 057
+        data    0100001      ; 060
+        data    0134077      ; 061
+        data    0001600      ; 062
+        data    0000000      ; 063
+        data    0000000      ; 064
+        data    0003021      ; 065
+        data    0002004      ; 066
+        data    0010071      ; 067
+        data    0010100      ; 070
+        data    0101031      ; 071
+        data    0000000      ; 072
+        data    0020400      ; 073
+        data    0021001      ; 074
+        data    0001002      ; 075
+        data    0000000      ; 076
+        data    0077775      ; 077
+
+        end     
---
+++ pymlac/pds4_expected_bootstrap.asm
@@ -0,0 +1,38 @@
+; ??? bootstrap code from file 'Sydney_University_PDS-4_Rom.png'
+; the values here are the 'Actual Contents' code
+        org     040
+
+        data    0104002      ; 040
+        data    0020035      ; 041
+        data    0100001      ; 042
+        data    0003000      ; 043
+        data    0001061      ; 044
+        data    0001454      ; 045
+        data    0010045      ; 046
+        data    0001051      ; 047
+        data    0030035      ; 050
+        data    0010043      ; 051
+        data    0110037      ; 052
+        data    0000002      ; 053
+        data    0000000      ; 054
+        data    0000000      ; 055
+        data    0000000      ; 056
+        data    0000000      ; 057
+        data    0104100      ; 060
+        data    0020036      ; 061
+        data    0060077      ; 062
+        data    0020010      ; 063
+        data    0004070      ; 064
+        data    0020037      ; 065
+        data    0104001      ; 066
+        data    0010041      ; 067
+        data    0074053      ; 070
+        data    0010064      ; 071
+        data    0034037      ; 072
+        data    0120010      ; 073
+        data    0030036      ; 074
+        data    0010072      ; 075
+        data    0134077      ; 076
+        data    0077677      ; 077
+
+        end     
