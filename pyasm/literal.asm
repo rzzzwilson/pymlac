676b5c3e707fcc8ad9ac8c6e7e96ab1f744d3ba5
Ross Wilson <rzzzwilson@gmail.com>
2016-10-27 21:54:50 +0700

    literal test code

---
+++ pyasm/literal.asm
@@ -0,0 +1,5 @@
+; a simple literal test file
+        org     0100
+start   lac     (data 0177777)
+        hlt
+        end     start
---
+++ pyasm/multiple_literal.asm
@@ -0,0 +1,7 @@
+; a multi-line literal test file
+        org     0100
+start   jms     (test   data    0
+                        law     0
+                        jmp     *test)
+        hlt
+        end     start
